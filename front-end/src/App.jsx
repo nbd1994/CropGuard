6e0950dcb8edf9c1356abd67432a00d941917667
Antiaastu <136442178+Antiaastu@users.noreply.github.com>
2024-07-21 01:20:29 +0300

    unity

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,9 @@
 import FormPage from "./FormPage"
+import Navbar from "./components/ui/Navbar";
 
 function App() {
   return <div className="flex justify-center bg-zinc-100 pt-10">
+    <Navbar />
     <FormPage />
   </div>
 }
